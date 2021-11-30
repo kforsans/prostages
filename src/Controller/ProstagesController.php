abd55c4e8e173ed7434ded93e9046a04e4f945d2
Kevin Forsans <kforsans@iutbayonne.univ-pau.fr>
2021-11-30 17:45:42 +0100

    Ajout des pages du TP1

--- src/Controller/ProstagesController.php
+++ src/Controller/ProstagesController.php
@@ -9,12 +9,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProstagesController extends AbstractController
 {
     /**
-     * @Route("/prostages", name="prostages")
+     * @Route("/", name="accueil")
      */
     public function index(): Response
     {
         return $this->render('prostages/index.html.twig', [
             'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+    public function entrepries(): Response
+    {
+        return $this->render('prostages/entreprises.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    /**
+     * @Route("/formations", name="formations")
+     */
+    public function formations(): Response
+    {
+        return $this->render('prostages/formations.html.twig', [
+            'controller_name' => 'ProstagesController',
+        ]);
+    }
+
+    /**
+     * @Route("/stage/{id}", name="stage-id")
+     */
+    public function stage($id): Response
+    {
+        return $this->render('prostages/stage-id.html.twig', [
+            'controller_name' => 'ProstagesController',
+            'id' => $id,
         ]);
     }
 }
