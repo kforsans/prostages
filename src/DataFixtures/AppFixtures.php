b4d712091bc3fb20cf0065914ca3dac870054410
Kevin Forsans <kforsans@iutbayonne.univ-pau.fr>
2022-01-13 15:39:41 +0100

    Injection formations fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,8 +12,24 @@ class AppFixtures extends Fixture
     {
         $dutInfo = new Formation();
         $dutInfo->setNomCourt("DUT Info");
-        $dutInfo->setNomLong("Diplome Universitaire de Technologie en Informatique");
+        $dutInfo->setNomLong("Diplome Universitaire de Technologie en informatique");
         $manager->persist($dutInfo);
+        
+        $dutGIM = new Formation();
+        $dutGIM->setNomCourt("DUT GIM");
+        $dutGIM->setNomLong("Diplome Universitaire de Technologie en génie industriel et maintenance");
+        $manager->persist($dutGIM);
+        
+        $lpProgAv = new Formation();
+        $lpProgAv->setNomCourt("LP Prog-Av");
+        $lpProgAv->setNomLong("Licence professionnelle en programmation avancée");
+        $manager->persist($lpProgAv);
+        
+        $lpNum = new Formation();
+        $lpNum->setNomCourt("LP Num");
+        $lpNum->setNomLong("Licence professionnelle de métiers du numérique");
+        $manager->persist($lpNum);
+        
         $manager->flush();
     }
 }
